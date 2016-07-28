d87b884ad8666741e54feddd385d33a36e1f62ae
박상철 <bad79s@lethe.kr>
2016-07-28 23:59:55 +0900

    etcd:  - Add docker-cloud.yml  - Change env name "ETC_INFRA" to "DOCKER_INFRA"  - Add dockercloud auth. ( Token, API key, Password)

--- alpine/3.4/etcd/docker-entrypoint.sh
+++ alpine/3.4/etcd/docker-entrypoint.sh
@@ -1,27 +1,32 @@
 #!/bin/sh
 
-ETCD_PORT_2379=${ETCD_PORT_2379:-2379}
-ETCD_PORT_2380=${ETCD_PORT_2380:-2380}
-ETCD_PORT_4001=${ETCD_PORT_2379:-4001}
-
-if [ "$ETCD_INFRA" = "AWS" ]; then
+if [ "$DOCKER_INFRA" = "AWS" ]; then
   ETCD_IP_V4=$(curl http://169.254.169.254/latest/meta-data/local-ipv4);
   ETCD_INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id);
-elif [ "$ETCD_INFRA" = "DOCKERCLOUD" ] then
-  if [ -z $DOCKERCLOUD_USERNAME -o -z $DOCKERCLOUD_PASSWORD ]; then
-    echo "DOCKERCLOUD_USERNAME: <your docker cloud id>"
-    echo "DOCKERCLOUD_PASSWORD: <your docker cloud password>"
-    exit 1
+elif [ "$DOCKER_INFRA" = "DOCKERCLOUD" ]; then
+  apk add --no-cache jq
+  CONTAINER_FILE="container.json"
+  DOCKERCLOUD_PASS=${DOCKERCLOUD_PASS:-$DOCKERCLOUD_APIKEY}
+
+  if [ "$DOCKERCLOUD_AUTH" ]; then
+    curl -H "Authorization: $DOCKERCLOUD_AUTH" $DOCKERCLOUD_CONTAINER_API_URL > $CONTAINER_FILE
+  elif [ "$DOCKERCLOUD_USER" -o "$DOCKERCLOUD_PASS" ]; then
+    curl -u "$DOCKERCLOUD_USER:$DOCKERCLOUD_PASS" $DOCKERCLOUD_CONTAINER_API_URL > $CONTAINER_FILE
   else
-    CONTAINER_FILE="container.json"
-    curl -u "$DOCKERCLOUD_USERNAME:$DOCKERCLOUD_PASSWORD" $DOCKERCLOUD_CONTAINER_API_URL > $CONTAINER_FILE
-    ETCD_IP_V4=$(jq -r '.private_ip' $CONTAINER_FILE)
-    ETCD_INSTANCE_ID=$(jq -r '.hostname' $CONTAINER_FILE)
-    ETCD_PORT_2379=$(jq -r '.link_variables as $env | .link_variables | keys | .[] | select(endswith("2379_TCP_PORT")) as $name | $env[$name]' $CONTAINER_FILE)
-    ETCD_PORT_2380=$(jq -r '.link_variables as $env | .link_variables | keys | .[] | select(endswith("2380_TCP_PORT")) as $name | $env[$name]' $CONTAINER_FILE)
-    ETCD_PORT_4001=$(jq -r '.link_variables as $env | .link_variables | keys | .[] | select(endswith("4001_TCP_PORT")) as $name | $env[$name]' $CONTAINER_FILE)
-    rm -rf $CONTAINER_FILE
+    echo "DOCKERCLOUD_USER: <your docker cloud id>"
+    echo "DOCKERCLOUD_PASS: <your docker cloud password>"
+    exit 1
   fi
+
+  ETCD_IP_V4=$(jq -r '.private_ip' $CONTAINER_FILE)
+  ETCD_INSTANCE_ID=$(jq -r '.hostname' $CONTAINER_FILE)
+  ETCD_PORT_2379=$(jq -r '.container_ports[]? | select(.inner_port == 2379 and .outer_port != null) | .outer_port' $CONTAINER_FILE)
+  ETCD_PORT_2380=$(jq -r '.container_ports[]? | select(.inner_port == 2380 and .outer_port != null) | .outer_port' $CONTAINER_FILE)
+  ETCD_PORT_4001=$(jq -r '.container_ports[]? | select(.inner_port == 4001 and .outer_port != null) | .outer_port' $CONTAINER_FILE)
+
+  rm -rf $CONTAINER_FILE
+
+  apk del jq
 else
   ETCD_IP_V4=$(hostname -i)
   ETCD_INSTANCE_ID=$(hostname)
@@ -37,11 +42,15 @@ if [ -z $ETCD_TOKEN ]
   echo "https://coreos.com/os/docs/latest/cluster-discovery.html"
   exit 1
 else
+  ETCD_PORT_2379=${ETCD_PORT_2379:-2379}
+  ETCD_PORT_2380=${ETCD_PORT_2380:-2380}
+  ETCD_PORT_4001=${ETCD_PORT_4001:-4001}
+
   etcd \
     -name $ETCD_INSTANCE_ID \
     -advertise-client-urls http://$ETCD_IP_V4:$ETCD_PORT_2379,http://$ETCD_IP_V4:$ETCD_PORT_4001 \
-    -listen-client-urls http://0.0.0.0:$ETCD_PORT_2379,http://0.0.0.0:$ETCD_PORT_4001 \
+    -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 \
     -initial-advertise-peer-urls http://$ETCD_IP_V4:$ETCD_PORT_2380 \
-    -listen-peer-urls http://0.0.0.0:$ETCD_PORT_2380 \
+    -listen-peer-urls http://0.0.0.0:2380 \
     -discovery https://discovery.etcd.io/$ETCD_TOKEN
 fi
