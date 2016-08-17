633de2a3e861033eb799a05cc4e30059307fd068
박상철 <bad79s@lethe.kr>
2016-08-17 18:25:06 +0900

    Change environment variable names for kafka.  - KAFKA_PORT to KAFKA_CLIENT_PORT

--- alpine/3.4/kafka/docker-entrypoint.sh
+++ alpine/3.4/kafka/docker-entrypoint.sh
@@ -1,15 +1,15 @@
-#!/bin/bash -x
+#!/bin/bash
 
 # If a ZooKeeper container is linked with the alias `zookeeper`, use it.
 # You MUST set ZOOKEEPER_IP in env otherwise.
-[ -n "$ZOOKEEPER_PORT_2181_TCP_ADDR" ] && ZOOKEEPER_IP=$ZOOKEEPER_PORT_2181_TCP_ADDR
-[ -n "$ZOOKEEPER_PORT_2181_TCP_PORT" ] && ZOOKEEPER_PORT=$ZOOKEEPER_PORT_2181_TCP_PORT
+[[ -n "$ZOOKEEPER_PORT_2181_TCP_ADDR" ]] && ZOOKEEPER_IP=$ZOOKEEPER_PORT_2181_TCP_ADDR
+[[ -n "$ZOOKEEPER_PORT_2181_TCP_PORT" ]] && ZOOKEEPER_PORT=$ZOOKEEPER_PORT_2181_TCP_PORT
 
-IP=$(cat /etc/hosts | head -n1 | awk '{print $1}')
+KAFKA_HOSTNAME=`hostname`
 
 # Concatenate the IP:PORT for ZooKeeper to allow setting a full connection
 # string with multiple ZooKeeper hosts
-[ -z "$ZOOKEEPER_CONNECTION_STRING" ] && ZOOKEEPER_CONNECTION_STRING="${ZOOKEEPER_IP}:${ZOOKEEPER_PORT:-2181}"
+[[ -z "$ZOOKEEPER_CONNECTION_STRING" ]] && ZOOKEEPER_CONNECTION_STRING="${ZOOKEEPER_IP}:${ZOOKEEPER_PORT:-2181}"
 
 cat ${KAFKA_HOME}/config/log4j.properties.template | sed \
   -e "s|{{KAFKA_HOME}}|${KAFKA_HOME:-/opt/kafka}|g" \
@@ -18,9 +18,9 @@ cat ${KAFKA_HOME}/config/log4j.properties.template | sed \
 cat ${KAFKA_HOME}/config/server.properties.template | sed \
   -e "s|{{ZOOKEEPER_CONNECTION_STRING}}|${ZOOKEEPER_CONNECTION_STRING}|g" \
   -e "s|{{ZOOKEEPER_CHROOT}}|${ZOOKEEPER_CHROOT:-}|g" \
-  -e "s|{{KAFKA_BROKER_ID}}|${KAFKA_BROKER_ID:-0}|g" \
-  -e "s|{{KAFKA_ADVERTISED_HOST_NAME}}|${KAFKA_ADVERTISED_HOST_NAME:-$IP}|g" \
-  -e "s|{{KAFKA_PORT}}|${KAFKA_PORT:-9092}|g" \
+  -e "s|{{KAFKA_BROKER_ID}}|${KAFKA_BROKER_ID:--1}|g" \
+  -e "s|{{KAFKA_ADVERTISED_HOST_NAME}}|${KAFKA_ADVERTISED_HOST_NAME:-$KAFKA_HOSTNAME}|g" \
+  -e "s|{{KAFKA_CLIENT_PORT}}|${KAFKA_CLIENT_PORT:-9092}|g" \
   -e "s|{{KAFKA_ADVERTISED_PORT}}|${KAFKA_ADVERTISED_PORT:-9092}|g" \
   -e "s|{{KAFKA_DELETE_TOPIC_ENABLE}}|${KAFKA_DELETE_TOPIC_ENABLE:-false}|g" \
   -e "s|{{KAFKA_HOME}}|${KAFKA_HOME:-/opt/kafka}|g" \
@@ -39,7 +39,7 @@ cat ${KAFKA_HOME}/config/server.properties.template | sed \
 # hosts running in a VM with Docker Machine, etc. See:
 #
 # https://issues.apache.org/jira/browse/CASSANDRA-7087
-if [ -z $KAFKA_JMX_OPTS ]; then
+if [[ -z "$KAFKA_JMX_OPTS" ]]; then
     KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremote=true"
     KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Dcom.sun.management.jmxremote.authenticate=false"
     KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Dcom.sun.management.jmxremote.ssl=false"
