e5410c7d2863e8876c3c2ae709e6c481ad7f7a19
박상철 <bad79s@lethe.kr>
2016-08-24 05:51:58 +0900

    Changes to show the logs without using the console command in the emqttd.

---
+++ alpine/3.4/emqttd/docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [[ "$1" == "start-with-log" ]]; then
+  emqttd start
+
+  EMQTTD_LOG_PATH=$EMQTTD_HOME/log
+  for LOG_LEVEL in debug info notice warning error critical alert emergency crash; do
+    LOG_FILE_PATH="${EMQTTD_LOG_PATH}/emqttd_${LOG_LEVEL}.log"
+    
+    if [[ -f "$LOG_FILE_PATH" ]]; then
+      LOG_FILE="$LOG_FILE_PATH"
+      break;
+    fi
+  done
+
+  if [[ -n "$LOG_FILE" ]]; then
+    echo "watch for $LOG_FILE"
+    tail -f $LOG_FILE
+  else
+    tail -f /dev/null
+  fi
+else
+  emqttd $@
+fi
