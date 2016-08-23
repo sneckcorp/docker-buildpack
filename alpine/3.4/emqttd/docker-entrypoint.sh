734d5c406375d630fe230f9e9e1fdf78aba4971f
박상철 <bad79s@lethe.kr>
2016-08-24 06:09:37 +0900

    Add log message in the emqttd entrypoint.

--- alpine/3.4/emqttd/docker-entrypoint.sh
+++ alpine/3.4/emqttd/docker-entrypoint.sh
@@ -6,7 +6,7 @@ if [[ "$1" == "start-with-log" ]]; then
   EMQTTD_LOG_PATH=$EMQTTD_HOME/log
   for LOG_LEVEL in debug info notice warning error critical alert emergency crash; do
     LOG_FILE_PATH="${EMQTTD_LOG_PATH}/emqttd_${LOG_LEVEL}.log"
-    
+
     if [[ -f "$LOG_FILE_PATH" ]]; then
       LOG_FILE="$LOG_FILE_PATH"
       break;
@@ -14,7 +14,8 @@ if [[ "$1" == "start-with-log" ]]; then
   done
 
   if [[ -n "$LOG_FILE" ]]; then
-    echo "watch for $LOG_FILE"
+    echo "watching the file(${LOG_FILE})"
+    sleep 1
     tail -f $LOG_FILE
   else
     tail -f /dev/null
