7f6223e4d9774ce09210f71006c54cb9c1eca6b4
박상철 <bad79s@lethe.kr>
2016-08-26 06:49:38 +0900

    Added an environment variable "EMQTTD_AUTO_CLUSTER_JOIN", "EMQTTD_NODE_NAME" and "EMQTTD_BIND_NODE_NAME".  - EMQTTD_AUTO_CLUSTER_JOIN : Automatically join the cluster. (boolean)  - EMQTTD_NODE_NAME : Set the "name" of vm.args. (string)  - EMQTTD_BIND_NODE_NAME : Set the "name" of vm.args. (enum) (ip or hostname)

--- alpine/3.4/emqttd/docker-entrypoint.sh
+++ alpine/3.4/emqttd/docker-entrypoint.sh
@@ -1,7 +1,43 @@
 #!/bin/sh
 
+auto_cluster_join() {
+  if [[ "$EMQTTD_AUTO_CLUSTER_JOIN" == "true" ]]; then
+    NODE_NAME=`egrep '^\-\s*name' $EMQTTD_HOME/etc/vm.args | sed -r 's/^-\s*name\s*(.*)/\1/'`
+    MASTER_NODE_PATH="$EMQTTD_HOME/cluster/master"
+
+    if [[ -f "$MASTER_NODE_PATH" ]]; then
+      MASTER_NAME=`cat $EMQTTD_HOME/cluster/master`
+      emqttd_ctl cluster join $MASTER_NAME &> /dev/null
+
+      echo "cluster mode: slave"
+      emqttd_ctl cluster status
+    else
+      mkdir -p $EMQTTD_HOME/cluster
+      echo "$NODE_NAME" > $MASTER_NODE_PATH
+
+      echo "cluster mode: master"
+    fi
+  fi
+}
+
+if [[ -z "$EMQTTD_NODE_NAME" -a -n "$EMQTTD_BIND_NODE_NAME" ]]; then
+  if [[ "$EMQTTD_BIND_NODE_NAME" == "ip" ]]; then
+    EMQTTD_NODE_NAME=`ip -4 addr show scope global dev eth0 | grep inet | head -1 | awk '{print \$2}' | cut -d / -f 1`
+  else
+    EMQTTD_NODE_NAME=`hostname -f`
+  fi
+fi
+
+if [[ -n "$EMQTTD_NODE_NAME" ]]; then
+  VM_ARGS_DATA=`cat $EMQTTD_HOME/etc/vm.args`
+  echo "$VM_ARGS_DATA" | sed "s/^-\s*name.*/-name emqttd@${EMQTTD_NODE_NAME}/" > $EMQTTD_HOME/etc/vm.args
+fi
+
 if [[ "$1" == "start-with-log" ]]; then
   emqttd start
+  auto_cluster_join
+
+  sleep 1
 
   EMQTTD_LOG_PATH=$EMQTTD_HOME/log
   for LOG_LEVEL in debug info notice warning error critical alert emergency crash; do
@@ -13,13 +49,15 @@ if [[ "$1" == "start-with-log" ]]; then
     fi
   done
 
+
   if [[ -n "$LOG_FILE" ]]; then
     echo "watching the file(${LOG_FILE})"
-    sleep 1
     tail -f $LOG_FILE
   else
+    echo "not found log file."
     tail -f /dev/null
   fi
 else
   emqttd $@
+  auto_cluster_join
 fi
